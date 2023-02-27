9a9160f51fbd6a4123acd68345082ec49dd327cd
Agus Utomo <agusutomo@kinsta.com>
2023-02-27 07:29:10 +0700

    added index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+img {
+  width: 100%;
+}
+</style>
+</head>
+<body>
+
+<img src="gajian.gif" alt="Animated gif" width="128" height="128">
+
+<img src="Android_Logo_09.png" alt="Android logo png" style="width:128px;height:128px;">
+
+</body>
+</html>
+';
+
+
+?>
